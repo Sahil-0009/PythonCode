ec80e378e8336af3c2e30cb6a9ad0f49f9fcf58a
Sahil <rcjubair786@gmail.com>
2024-06-03 20:48:31 +0530

    add python file

---
+++ hello.py
@@ -0,0 +1,73 @@
+# for var1 in range(1,6):
+#     for var2 in range(1,6):
+#         if(var1%var2 !=0):
+#             pass
+#         elif(var2<var1):
+#             continue
+#         else :
+#             print(var1*var2)
+
+# ship_crew={
+#     "Co-Captain":"Jack",
+#     "Chief officer":"Mack",
+#     "Chief steward":"Harry",
+#     "Cheif cook":"Mala" 
+# }
+
+
+num1=11//10
+num2=11%10
+num3=20
+num4=40
+num5=5
+
+# if(num3>num4):
+#     if (num3> num5):
+#         print(num5*num4/num3)
+#     else :
+#         print(num3/num5)
+# else:
+#     if(num1 == num2) :
+#         print(num4/num3)
+#     else :
+#         print(num4/num5)    
+
+
+# my_str="All3 that4 glitters8 is 2 not3 gold4"
+# my_lst=[]
+# for char in my_str:
+#     if(char.isdigit()):
+#         my_lst.append((int)(char))
+#         my_str=my_str.replace(char," ")
+# print(my_str,my_lst)
+# for char in my_str:
+#     if(char.isdigit()):
+#         my_lst.append(char)
+#         my_str.replace(char," ")
+# print(my_str,my_lst)
+
+
+var = 200
+if (var › 200):
+    print("Within first block")
+    if (var == 150):
+          print( "which is 150")
+    elif (var == 100):
+            print( "which is 100")
+elif (var › 50):
+    print("Within second block")
+    if (var%5 == 0):
+        print("which is multiple of 5")
+    elif (var%10 == 0):
+        print("Which is multiple of 10")
+    else:
+        print("Neither multiple of 5 nor multiple of 10")
+else:
+    print("Good bye! ")
+print("Could not find")
+temp=[ 'Mysore', 'Bangalore', 'Pune', ' temp.sort()
+count1=len (temp [0])
+count2=len(temp[-1])
+final_val=math.ceil (count1/count2)
+print (final_val)
+  
